d5038e68e9e0fefb5a3133e5494c67294d4c70b9
nikitabhande1721 <nikitabhande.rkinfynova@gmail.com>
2025-12-28 12:20:28 +0530

    first commit

---
+++ mobile_app/edscore-app-teacher/lib/presentation/bloc/auth/reset/reset_state.dart
@@ -0,0 +1,22 @@
+abstract class ResetPasswordState {}
+
+class ResetPasswordInitial extends ResetPasswordState {}
+
+class ResetPasswordLoading extends ResetPasswordState {}
+
+class ResetPasswordSuccess extends ResetPasswordState {}
+
+class ResetPasswordFailure extends ResetPasswordState {
+  final String error;
+  ResetPasswordFailure(this.error);
+}
+
+class ResetPasswordValidating extends ResetPasswordState {
+  final String? newPasswordError;
+  final String? confirmPasswordError;
+
+  ResetPasswordValidating({
+    this.newPasswordError,
+    this.confirmPasswordError,
+  });
+}
