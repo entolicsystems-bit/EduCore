bc6adc58b8146eb2a0a952347eb1168b5df0713c
Shreyas <patilshreyas547@gmail.com>
2025-12-29 18:25:22 +0530

    day update

--- backend/src/modules/leads/leads.service.ts
+++ backend/src/modules/leads/leads.service.ts
@@ -55,7 +55,7 @@ export class LeadsService {
       },
     });
 
-    return { success: true };
+    return lead;
   }
 
   getLeads(filters: LeadFilterDto) {
--- backend/src/modules/roles/roles.service.ts
+++ backend/src/modules/roles/roles.service.ts
@@ -26,43 +26,42 @@ export class RolesService {
   }
 
   async registerStaff(dto: RegisterDto) {
-    try {
-      const email = dto.email;
-      const name = dto.name;
-      const phone = dto.phone;
-      const password = dto.password;
-      const roleName = dto.role.toUpperCase();
-      const existingUser = await this.prisma.user.findUnique({
-        where: { email },
-      });
-      if (existingUser) {
-        throw new BadRequestException("User already exists");
-      }
-
-      const hashedPassword = await bcrypt.hash(password, 10);
-
-      const user = await this.prisma.user.create({
-        data: {
-          email,
-          name,
-          phone,
-          role: roleName,
-          passwordHash: hashedPassword,
-        },
-      });
-      if (roleName === "COUNSELLOR") this.assignRole(user.id, 2);
-      else if (roleName === "TEACHER") this.assignRole(user.id, 3);
-      else if (roleName === "ACCOUNTANT") this.assignRole(user.id, 4);
-      else throw new BadRequestException("Invalid Role");
-      
-      const { passwordHash: _, ...result } = user;
-
-      return result;
-    } catch (error) {
-      throw new InternalServerErrorException(
-        console.log(error),
-        "Error while creating User"
-      );
+    const email = dto.email;
+    const name = dto.name;
+    const phone = dto.phone;
+    const password = dto.password;
+    const roleName = dto.role.toUpperCase();
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email },
+    });
+    if (existingUser) {
+      throw new BadRequestException("User already exists");
     }
+    const existingPhone = await this.prisma.user.findFirst({
+      where: { phone },
+    });
+    if (existingPhone) {
+      throw new BadRequestException("PhoneNo already exists");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.prisma.user.create({
+      data: {
+        email,
+        name,
+        phone,
+        role: roleName,
+        passwordHash: hashedPassword,
+      },
+    });
+    if (roleName === "COUNSELLOR") this.assignRole(user.id, 2);
+    else if (roleName === "TEACHER") this.assignRole(user.id, 3);
+    else if (roleName === "ACCOUNTANT") this.assignRole(user.id, 4);
+    else throw new BadRequestException("Invalid Role");
+
+    const { passwordHash: _, ...result } = user;
+
+    return result;
   }
 }
