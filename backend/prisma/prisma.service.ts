8c07f343d66c03b1fa7772b1761fadba4039cd1b
sejalgholaprkinfynova-hub <sejalgholap.rkinfynova@gmail.com>
2026-01-05 11:56:14 +0530

    Update prisma.service.ts

--- backend/prisma/prisma.service.ts
+++ backend/prisma/prisma.service.ts
@@ -1,17 +1,31 @@
+// src/database/prisma.service.ts
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { Injectable } from '@nestjs/common';
-import * as config from 'config'; 
+import * as config from 'config';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
     super({
       datasources: {
         db: {
-          url: config.get<string>("DATABASE_URL"), 
+          url: config.get<string>('DATABASE_URL'), // dynamically set database URL
         },
       },
-      log: ['error', 'warn','query'],
+      log: ['error', 'warn', 'query'], // optional: include 'query' for debugging
     });
+  }
+
+  // Connect when the module initializes
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  // Disconnect gracefully when the module is destroyed
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
