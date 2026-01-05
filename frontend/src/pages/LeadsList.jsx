aad1dfc7b269bf9439e393fa8bf6308384fa4a3c
sejalgholaprkinfynova-hub <sejalgholap.rkinfynova@gmail.com>
2026-01-05 09:20:02 +0530

    Update LeadsList.jsx

--- frontend/src/pages/LeadsList.jsx
+++ frontend/src/pages/LeadsList.jsx
@@ -27,13 +27,10 @@ const Leads = () => {
       setLeads(storedLeads);
 
       const uniqueOwners = [
-<<<<<<< HEAD
         ...new Set(
           storedLeads.map((lead) => lead.owner).filter(Boolean)
         ),
-=======
         ...new Set(storedLeads.map((lead) => lead.owner).filter(Boolean)),
->>>>>>> 5aac09e3570f67aa3f52c483ad0569c079da4515
       ];
       setOwners(uniqueOwners);
     };
@@ -54,7 +51,6 @@ const Leads = () => {
   /* RESET PAGE ON FILTER CHANGE */
   useEffect(() => {
     setCurrentPage(1);
-<<<<<<< HEAD
   }, [statusFilter, sourceFilter,ownerFilter, search]);
 
   /* FILTER */
@@ -68,8 +64,6 @@ const Leads = () => {
     const ownerMatch =
       ownerFilter === "ALL" || lead.owner === ownerFilter;
 
-     
-=======
   }, [statusFilter, sourceFilter, ownerFilter, search]);
 
   /* FILTER */
@@ -79,7 +73,6 @@ const Leads = () => {
     const sourceMatch = sourceFilter === "ALL" || lead.source === sourceFilter;
 
     const ownerMatch = ownerFilter === "ALL" || lead.owner === ownerFilter;
->>>>>>> 5aac09e3570f67aa3f52c483ad0569c079da4515
 
     const searchText = search.toLowerCase();
     const searchMatch =
@@ -116,32 +109,27 @@ const Leads = () => {
   return (
     <DashboardLayout>
       <div className="leads-page">
-<<<<<<< HEAD
-
-        {/* HEADER */}
+     {/* HEADER */}
         <div className="leads-header">
           <h2>Leads</h2>
           <div className="header-actions">
             <button
               className="btn-outline"
-=======
+
         {/* HEADER */}
         <div className="leads-header">
           <h2 className="font-bold">Leads</h2>
           <div className="header-actions">
             <button
               className="border-2 border-[#0d99ff] text-[#0d99ff] px-6 py-2 rounded-md cursor-pointer"
->>>>>>> 5aac09e3570f67aa3f52c483ad0569c079da4515
               onClick={() => navigate("/leads/import")}
             >
               Import CSV
             </button>
             <button
-<<<<<<< HEAD
+
               className="btn-primary"
-=======
               className=" bg-[#0d99ff] text-white px-6 py-2 rounded-md cursor-pointer"
->>>>>>> 5aac09e3570f67aa3f52c483ad0569c079da4515
               onClick={() => navigate("/leads/create")}
             >
               Create Lead
@@ -165,17 +153,13 @@ const Leads = () => {
             <option value="Social Media">Social Media</option>
             <option value="Email Campaign">Email Campaign</option>
           </select>
-
-<<<<<<< HEAD
           <select 
              value={ownerFilter}
              onChange={(e) => setSourceFilter(e.target.value)}>
-=======
           <select
             value={ownerFilter}
             onChange={(e) => setSourceFilter(e.target.value)}
           >
->>>>>>> 5aac09e3570f67aa3f52c483ad0569c079da4515
             <option value="ALL">All Owner</option>
             {owner.map((owner, index) => (
               <option key={index} value={owner}>
@@ -292,7 +276,6 @@ const Leads = () => {
         </div>
 
         {/* PAGINATION */}
-<<<<<<< HEAD
         
           <div className="pagination">
             <span>
@@ -325,8 +308,6 @@ const Leads = () => {
               </button>
             </div>
           </div>
-        
-=======
 
         <div className="pagination">
           <span>
@@ -359,7 +340,6 @@ const Leads = () => {
             </button>
           </div>
         </div>
->>>>>>> 5aac09e3570f67aa3f52c483ad0569c079da4515
       </div>
     </DashboardLayout>
   );
