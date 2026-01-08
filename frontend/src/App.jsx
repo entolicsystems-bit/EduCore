f256903cdc155de0064250f4d45fde01f3a4cd41
pravindhamalrkinfynova <pravindhamal.rkinfynova@gmail.com>
2026-01-08 14:27:27 +0530

    role wise api integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         {/* PUBLIC ROUTES */}
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgot" element={<ForgotPassword />} />
         <Route path="/otp" element={<Otp />} />
--- frontend/src/services/leadService.js
+++ frontend/src/services/leadService.js
@@ -19,3 +19,16 @@ export const updateLead = (id, data) => {
 export const deleteLead = (id) => {
   return API.delete(`/v1/leads/${id}`);
 };
+
+
+// IMPORT LEADS CSV
+export const importLeadsCSV = (file) => {
+  const formData = new FormData();
+  formData.append("file", file); // backend expects key "file"
+
+  return API.post("/v1/csv/import", formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
