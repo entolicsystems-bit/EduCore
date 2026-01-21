f77f58d5a97ebd77f9115e4bfd164a724713cf30
gauravkondhalkarrkinfynova-netizen <gauravkondhalkar.rkinfynova@gmail.com>
2026-01-21 15:11:20 +0530

    Update before_install.sh

--- backend/scripts/before_install.sh
+++ backend/scripts/before_install.sh
@@ -1,6 +1,18 @@
+# #!/bin/bash
+# echo "Running BeforeInstall..."
+# set -e
+# rm -rf /home/ec2-user/EduCore/backend
+# mkdir -p /home/ec2-user/EduCore/backend
 #!/bin/bash
 echo "Running BeforeInstall..."
 set -e
-rm -rf /home/ec2-user/EduCore/backend
+
+# ensure folder exists
 mkdir -p /home/ec2-user/EduCore/backend
+
+# delete everything except .env
+find /home/ec2-user/EduCore/backend \
+  -mindepth 1 \
+  ! -name ".env" \
+  -exec rm -rf {} +
 
