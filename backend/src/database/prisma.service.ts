eea0e9305d8188db24a30625c34147cf995dc5a4
gauravkondhalkarrkinfynova-netizen <gauravkondhalkar.rkinfynova@gmail.com>
2026-01-21 15:28:32 +0530

    Update prisma.service.ts

--- backend/src/database/prisma.service.ts
+++ backend/src/database/prisma.service.ts
@@ -1,25 +1,58 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
-import { PrismaClient } from "@prisma/client";
-import { ConfigService } from "@nestjs/config";
+// import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common";
+// import { PrismaClient } from "@prisma/client";
+// import { ConfigService } from "@nestjs/config";
+
+// @Injectable()
+// export class PrismaService
+//   extends PrismaClient
+//   implements OnModuleInit, OnModuleDestroy
+// {
+//   constructor(private readonly config: ConfigService) {
+//     super({
+//       accelerateUrl: config.get<string>("DATABASE_URL"),
+//     });
+//     // super();
+
+//   }
+
+//   async onModuleInit() {
+//     await this.$connect();
+//   }
+
+//   async onModuleDestroy() {
+//     await this.$disconnect();
+//   }
+// }
+
+
+import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
-  constructor(private readonly config: ConfigService) {
-    super({
-      accelerateUrl: config.get<string>("DATABASE_URL"),
-    });
-    // super();
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    const pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }, // RDS SSL recommended
+    })
 
+    const adapter = new PrismaPg(pool)
+
+    super({
+      adapter,
+      log: ['warn', 'error'],
+    })
   }
 
   async onModuleInit() {
-    await this.$connect();
+    await this.$connect()
   }
 
   async onModuleDestroy() {
-    await this.$disconnect();
+    await this.$disconnect()
   }
 }
+
+//   }
+// }
