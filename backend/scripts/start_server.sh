65d92efa5fbc499e8eab8b4bf00013b7b2e260e6
gauravkondhalkarrkinfynova-netizen <gauravkondhalkar.rkinfynova@gmail.com>
2026-01-21 14:20:04 +0530

    Update start_server.sh

--- backend/scripts/start_server.sh
+++ backend/scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/ec2-user/EduCore/backend || exit 1
 npm install
-npx prisma generate
+# npx prisma generate
 pm2 restart backend || pm2 start main.js --name backend
 pm2 save
 # cd /home/ec2-user/EduCore/backend
