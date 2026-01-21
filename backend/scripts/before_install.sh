f83c18261c19e844a8fec509abca5d13f7b4f9c3
gauravkondhalkarrkinfynova-netizen <gauravkondhalkar.rkinfynova@gmail.com>
2026-01-21 19:20:29 +0530

    Update before_install.sh

--- backend/scripts/before_install.sh
+++ backend/scripts/before_install.sh
@@ -5,6 +5,10 @@
 # mkdir -p /home/ec2-user/EduCore/backend
 #!/bin/bash
 echo "BeforeInstall..."
+
+pm2 delete all || true
+pm2 kill || true
+
 # keep .env safe
 mv /home/ec2-user/EduCore/backend/.env /tmp/backend.env 2>/dev/null || true
 
