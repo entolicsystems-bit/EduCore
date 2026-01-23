55936da499b4d99cd475084950d69c5a3e255e25
gauravkondhalkarrkinfynova-netizen <gauravkondhalkar.rkinfynova@gmail.com>
2026-01-23 16:03:09 +0530

    Update start_server.sh

--- backend/scripts/start_server.sh
+++ backend/scripts/start_server.sh
@@ -58,6 +58,10 @@ npx prisma generate
 # 1️⃣ MIGRATION
 echo "📦 Running Prisma migrate deploy..."
 DATABASE_URL="$DIRECT_DATABASE_URL"
+npx prisma migrate resolve --applied 20251218051251_make_email_unique
+
+
+
 npx prisma migrate deploy --schema prisma/schema.prisma || {
   echo "❌ Migration failed"
   exit 1
